75b843cf7cc2f16ba05ed35fe7a81af90df579b7
Sayed Tauseef Naqvi <tauseefnaqvi@Sayeds-MacBook-Air.local>
2017-12-18 12:16:33 +0530

    test find query

--- modules/transaction/controllers/transactionHistory.js
+++ modules/transaction/controllers/transactionHistory.js
@@ -88,7 +88,7 @@ let transactionHistory3 = (req, res) => {
 
 let transactionHistory = (req, res) => {
     let address = req.params.address;
-
+    let t = new Date();
     if (!address)
         return res.json({success: false, msg: "Address can't be null."});
 
@@ -103,18 +103,21 @@ let transactionHistory = (req, res) => {
     //find 'from' transactions of address
     Transaction.find({from: new RegExp(address, "i")}).lean()
         .then((fromTransactions) => {
+            console.log("In 1 time: "+(new Date()-t))
             if (!fromTransactions.length)
                 return "No transaction in from";
             //type:out inject in 'from' all transactions
             return projectUtils.injectKeyValueInArray(fromTransactions, {type: 'out', isPending: false})
         })
         .then((outTransactions) => {
+            console.log("In 2 time: "+(new Date()-t))
             if (outTransactions !== "No transaction in from")
                 allTransactions = outTransactions;
             //find 'to' transactions of address
             return Transaction.find({to: new RegExp(address, "i")}).lean();
         })
         .then((toTransactions) => {
+            console.log("In 3 time: "+(new Date()-t))
             if (!toTransactions.length) {
                 // sort transactions in descending order of timestamp
                 if (allTransactions.length)
@@ -128,11 +131,13 @@ let transactionHistory = (req, res) => {
             return projectUtils.injectKeyValueInArray(toTransactions, {type: 'in', isPending: false})
         })
         .then((inTransactions) => {
+            console.log("In 4 time: "+(new Date()-t))
             allTransactions = allTransactions.concat(inTransactions);
             // sort transactions in descending order of timestamp
             allTransactions = allTransactions.sort(function (a, b) {
                 return new Date(b.timestamp) - new Date(a.timestamp);
             });
+            console.log("In 5 time: "+(new Date()-t))
             return res.json({success: true, transactions: allTransactions});
         })
         .catch((error) => {
