9edaeedab280db1b1ce9a3f95c61309450fe12a4
Sayed Tauseef Naqvi <tauseefnaqvi@Sayeds-MacBook-Air.local>
2017-12-20 17:52:20 +0530

    change geth rpc address

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 const config = {
     'port': 8001,                                         // set ourport
     'database': 'mongodb://127.0.0.1:27017/blockchain-explorer',          // database connection link
-    'rpcUrl':'http://ec2-54-254-143-33.ap-southeast-1.compute.amazonaws.com:8545' //rpcUrl of geth node
+    'rpcUrl':'http://ec2-54-169-236-106.ap-southeast-1.compute.amazonaws.com:8545' //rpcUrl of geth node
 };
 module.exports = config;
